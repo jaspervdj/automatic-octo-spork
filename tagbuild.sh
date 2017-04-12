4a3d22fef08756fec2728052c110c615b2ae1a4f
Jasper Van der Jeugt <m@jaspervdj.be>
2017-04-12 23:40:10 +0200

    k

--- tagbuild.sh
+++ tagbuild.sh
@@ -12,12 +12,12 @@ if [[ "$TRAVIS" == "true" ]]; then
 fi
 
 echo $CI_BRANCH
-if [[ "$CI_BRANCH" =~ '^release/.*$' ]]; then
+if [[ "$CI_BRANCH" =~ ^release/.*$ ]]; then
   RELEASE="$(echo "$CI_BRANCH" | sed 's/^release\///')"
   TAG_NAME="v$RELEASE-$CI_BUILD"
   TAG_MESSAGE="Releasing $TAG_NAME today yo"
 else
-  echo "This is not a release, branch, skipping..."
+  echo "This is not a release branch, skipping..."
   exit 0
 fi
 
